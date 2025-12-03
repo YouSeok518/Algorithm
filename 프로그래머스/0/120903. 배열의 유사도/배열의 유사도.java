514ef57372e4dddc1c89bd5c4a1ffdef7f0a8bd1
Kim-You-Seok <luke2062@naver.com>
2025-12-03 21:59:09 +0900

    [level 0] Title: 배열의 유사도, Time: 0.03 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int count = 0;
+        for(String a : s1){
+        for(String b : s2){
+            if(a.equals(b)){
+                count++;
+            }
+        }
+    }
+        return count;
+    }
+}

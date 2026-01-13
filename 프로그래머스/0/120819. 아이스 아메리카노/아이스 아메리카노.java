b259d56b1fb0ce8eddf62b73f9255fb26c73100d
Kim-You-Seok <luke2062@naver.com>
2026-01-13 22:25:27 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 82.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int[] solution(int money) {
+        int coffee = money / 5500;
+        int change = money % 5500;
+        return new int[]{coffee , change};
+    }
+}
